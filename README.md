069f38893004e97bbcd8a6586ff20d9ae85dc90f
Igor Gulamov <igor.gulamov@gmail.com>
2019-09-11 17:39:20 -0400

    add coinselect

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # zcoinselect
 Coin selection library for 2 to 2 transactions and 2 to 1+1 withdrawals
+
+Select 2 closest by amount UTXOs, enough to process transfer. If transfer cannot be processed, the client needs to join 2 UTXOs with highest amount.
---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+exports = zcoinselect;
+
+const maxAmount = coins => coins.slice(-2).reduce((a, b) => a[1] + b[1]);
+
+
+function zcoinselect(coins, amount) {
+  amount = BigInt(amount);
+
+  coins = [[undefined, 0n], [undefined, 0n], ..._.sortBy(coins.map((e, i) => [i, e]), e => e[1])];
+
+  const MaxAmount = maxAmount(coins);
+  if (amount < MaxAmount)
+    return null;
+
+  const s = (i, d) => (coins[i][1] + coins[i + d][1]) >= amount;
+
+  let i = coins.length - 2;
+  let d = 1;
+  while (i >= 0) {
+    if (s(i - 1, d)) {
+      i -= 1;
+    } else if (s(i - 1, d + 1)) {
+      i -= 1;
+      d += 1;
+    } else break;
+  }
+  return [coins[i][0], coins[i + d][0]]
+}
