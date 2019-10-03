4f38b8a69543bf02997e5c6b810e12e7275a8f67
Igor Gulamov <igor.gulamov@gmail.com>
2019-10-03 18:03:53 +0300

    update for multiasset utxos

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # zcoinselect
 Coin selection library for 2 to 2 transactions and 2 to 1+1 withdrawals
 
-Select 2 closest by amount UTXOs, enough to process transfer. If transfer cannot be processed, the client needs to join 2 UTXOs with highest amount.
 
 ## Example
 
 ```js
-> const zcoinselect = require("zcoinselect");
-> zcoinselect([1n, 2n, 4n, 8n], 6n)
+> const z = require("zcoinselect");
+> z([[1n, 10n, 5n], [1n, 15n, 4n]], [1n, 20n, 4n], 1n)
 [1, 2]
 ```
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = zcoinselect;
 
 const maxAmount = coins => coins.slice(-2).reduce((a, b) => a[1] + b[1]);
 
-
+/*
 function zcoinselect(coins, amount) {
   amount = BigInt(amount);
 
@@ -30,4 +30,39 @@ function zcoinselect(coins, amount) {
     } else break;
   }
   return [coins[i][0], coins[i + d][0]].filter(e => typeof e !== "undefined")
+}*/
+
+/*
+Working fast with low number of utxos (<100). May be improved using topological sorting.
+
+
+coin = [assetId, assetAmount, nativeAmount]
+*/
+
+
+function zcoinselect(coins, amount, fee) {
+  amount = amount.map(x=>BigInt(x));
+  coins = coins.map(c=>c.map(x=>BigInt(x)));
+  fee = BigInt(fee);
+
+  let coins_enumerated = coins.map((c, i) => [...c, i]);
+
+  let not_enough = [true, true];
+  let best_result = [];
+
+  for(let i = 0; i<coins_enumerated.length; i++) {
+    const ce = coins_enumerated[i];
+    for (let j = 0; j<coins_enumerated.length; j++) {
+      const ca = coins_enumerated[j];
+      if (ce[3]==ca[3]) continue;
+      if ((ce[0]==amount[0] ? ce[1] : 0n) + (ca[0]==amount[0] ? ca[1] : 0n)  >= amount[2]) not_enough[0] = false;
+      if (ce[2] + ca[2] >= amount[2] + fee) not_enough[1] = false;
+      if ((ce[2] + ca[2] >= amount[2] ) && ((ce[0]==amount[0] ? ce[1] : 0n) + (ca[0]==amount[0] ? ca[1] : 0n)  >= amount[2])) {
+        return {result: [ce[3], ca[3]], not_enough};
+      }
+    }
+  }
+
+  return {result: [], not_enough};
+
 }
